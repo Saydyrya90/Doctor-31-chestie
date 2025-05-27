240ba1f7c427dc2a07d33f6aa80de6a4f67fa44f
Nemes Dragos <dragos.nemes@qubiz.com>
2025-05-27 18:37:25 +0300

    deleted unused folder

--- BigDataInitialSetupPython-main/generate-documentation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-export PATH="$HOME/.local/bin:$PATH"
-
-echo "📚 Generating HTML documentation with pdoc..."
-pdoc src -o docs
-
-echo "✅ Documentation ready at: docs/index.html"
--- BigDataInitialSetupPython-main/run-unit-tests-and-coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-export PYTHONPATH=src
-coverage run -m pytest tests/
-coverage report
-coverage html
